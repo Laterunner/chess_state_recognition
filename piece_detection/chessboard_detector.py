b7d107582bb6c3c7f5eaf7c1014432b938898544
marius <mebc@gmx.net>
2023-05-23 19:04:12 +0200

    save array corners to file

--- piece_detection/chessboard_detector.py
+++ piece_detection/chessboard_detector.py
@@ -23,20 +23,26 @@ def return_board_from_image(img, model, log = True, isRoboflow = False):
         return None
     
     np.save("cornersmb", corners)
-    print("array corners savedto file")
+    print("array corners saved to file")
     print()
     
-    ########### inserted some lines to print out and save four outer corners ###########
-    # corners clockwise starting upper left
+    ########### inserted some lines  print out outer and save array of corners to file ###########
+    # corners clockwise starting upper left (yx values)
     c1 = corners[0][0]
     c2 = corners[8][0]
     c3 = corners[8][8]
     c4 = corners[0][8]
 
-    #print("corner 1", c1)
-    #print("corner 2", c2)
-    #print("corner 3", c3)
-    #print("corner 4", c4)
+    ### if needed invert corners (xy):
+    c1inv = c1[::-1]
+    c2inv = c2[::-1]    
+    c3inv = c3[::-1]
+    c4inv = c4[::-1]
+
+    print("corner 1 y,x", c1)
+    print("corner 2 y,x", c2)
+    print("corner 3 y,x", c3)
+    print("corner 4 y,x", c4)
 
     img = cv2.circle(img, (c1[1], c1[0]), radius=3, color=(0, 255, 0), thickness= -1)
     img = cv2.circle(img, (c2[1], c2[0]), radius=3, color=(0, 255, 0), thickness= -1)
@@ -47,44 +53,10 @@ def return_board_from_image(img, model, log = True, isRoboflow = False):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
  
-    ### in Realtime-OPENCV-Chess needs inverted corners (xy) :
-    c1inv = c1[::-1]
-    c2inv = c2[::-1]    
-    c3inv = c3[::-1]
-    c4inv = c4[::-1]
-
-    l_top       = c1.tolist()
-    r_top       = c2.tolist()
-    r_bottom    = c3.tolist()
-    l_bottom    = c4.tolist() 
-
-    l_topinv    = c1inv.tolist()
-    r_topinv    = c2inv.tolist()
-    r_bottominv = c3inv.tolist()
-    l_bottominv = c4inv.tolist() 
-
-    ### clockwise corners x,y
-    L2 = l_topinv, r_topinv, r_bottominv, l_bottominv
-    ### clockwise corners y,x
-    L4 = l_top, r_top, r_bottom, l_bottom
-
-    ### Writing to myfile.txt
-    with open("myfile.txt", "w") as file1:
-        file1.write("Corners x,y 1-4 written to file\n")
-        file1.writelines(str(L2))
-        file1.writelines("\n")       
-        file1.write("Corners y,x 1-4 written to file\n")
-        file1.writelines(str(L4))
-
-    # Reading from file
-    with open("myfile.txt", "r+") as file1:
-        # Reading form a file
-        print(file1.read())
 
     ###########
     return  ###
     ###########
-
     
     squares = get_squares_from_corners(corners)
 
